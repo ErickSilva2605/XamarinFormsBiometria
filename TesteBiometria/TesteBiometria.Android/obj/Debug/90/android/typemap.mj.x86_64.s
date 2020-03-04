3509704c563595627bba8bfd04e5051ba4dd8b1c
Erick Augusto <erick.dev.it@gmail.com>
2020-03-03 21:56:14 -0300

    first commit

---
+++ TesteBiometria/TesteBiometria.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 97BBED7892517692 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1529
+	/* entry-length */
+	.long	259
+	/* value-offset */
+	.long	142
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 396012
+	.include	"typemap.mj.inc"
---
+++ TesteBiometria/TesteBiometria/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TesteBiometria
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private CancellationTokenSource _cancel;
+        private bool _initialized;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Autenticar(object sender, EventArgs e)
+        {
+            var result = await CrossFingerprint.Current.IsAvailableAsync(true);
+
+            if (result)
+            {
+                AuthenticationRequestConfiguration configuration = new AuthenticationRequestConfiguration("Toque no Sensor", "Acessar");
+
+                var auth = await CrossFingerprint.Current.AuthenticateAsync(configuration);
+                if (auth.Authenticated)
+                {
+                    Resultado.Text = "Autenticado com sucesso! :)";
+                }
+                else
+                {
+                    Resultado.Text = "Impressão digital não reconhecida";
+                }
+            }
+            else
+            {
+                await DisplayAlert("Ops", "Dispositivo não suportado", "OK");
+            }
+        }
+    }
+}
